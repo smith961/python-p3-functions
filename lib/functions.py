a1c8c2058202070eaaf599f89512e9cf588d1f59
smith <smithbabawande@gmail.com>
2023-09-28 12:52:59 +0100

    Completed Assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+    
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    
 
 def add(num1, num2):
-    pass
+    sum_num= num1 + num2
+    return sum_num
+    
 
 def halve(number):
-    pass
+    return number/2
+    
+
+# def stylish_painter():
+#     best_hairstyle = "Bob Ross"
+#     return "Jean-Michel Basquiat"
+#     return best_hairstyle
+#     print(best_hairstyle)
+
+# stylish_painter()
